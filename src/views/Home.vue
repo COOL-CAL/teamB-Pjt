b8a5befaaa5e2a08a9140efd46bc1d3a868e50ac
junirii <ga243@naver.com>
2022-07-22 10:37:41 +0900

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    message
+  </div>
+
+  <router-link :to="{ path: '/Map' }">
+    <button type="button">시작하기</button>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+}
+</script>
---
+++ src/views/Map.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h3>여행지 설정</h3>
+  </div>
+</template>
