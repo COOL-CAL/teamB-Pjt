e7636a3f9cb993f37855f44f944fed6a52cfa53e
YoonJeongIn <101864135+olnnl510@users.noreply.github.com>
2022-07-26 09:35:22 +0900

    option 화면 구현

--- team-B-project_front/src/views/Calender.vue
+++ team-B-project_front/src/views/Calender.vue
@@ -1,12 +1,36 @@
 <template>
-  <h3>날짜 설정</h3>
+  <h3>날짜 선택</h3>
   <div>
     달력 예정<br>
     출발일
-    <input type="date" name="startDay"><br>
+    <input type="date" name="startDay">
     도착일
     <input type="date" name="endDay">
+  </div>
+  <div>
+    <div class="calendar">
+      <div class="header">
+        <div class="year-month"></div>
+        <div class="nav">
+          <button class="nav-btn go-prev">&lt;</button>
+          <button class="nav-btn go-today">Today</button>
+          <button class="nav-btn go-next">&gt;</button>
+        </div>
+      </div>
+      <div class="main">
+        <div class="days">
+          <div class="day">일</div>
+          <div class="day">월</div>
+          <div class="day">화</div>
+          <div class="day">수</div>
+          <div class="day">목</div>
+          <div class="day">금</div>
+          <div class="day">토</div>
+        </div>
+        <div class="dates"></div>
+      </div>
     </div>
+  </div>
   <div>
     <router-link :to="{ path: '/List' }">
       <button type="button">여행 찾기</button>
@@ -21,5 +45,4 @@ export default {
 </script>
 
 <style>
-
 </style>
